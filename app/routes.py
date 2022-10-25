e83498652b100d598810606a05aa5d3ba8f551cd
Leimomi Bong <leimomibong@gmail.com>
2022-10-24 20:01:58 -1000

    progress in activity

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from curses import BUTTON2_DOUBLE_CLICKED
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -17,6 +18,18 @@ books = [
 hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -30,8 +43,19 @@ def handle_books():
         )
     return jsonify(books_response)
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
 
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+# ////
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
     my_beautiful_world = "Hello, world!"
     return my_beautiful_world, 200
